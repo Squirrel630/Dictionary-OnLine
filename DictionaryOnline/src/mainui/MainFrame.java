71baa8efcc022249ad67948beace6775585904d0
Squirrel630 <847488290@qq.com>
2016-11-28 23:59:57 +0800

    local dictionary
    the original version.

--- DictionaryOnline/src/mainui/MainFrame.java
+++ DictionaryOnline/src/mainui/MainFrame.java
@@ -20,7 +20,7 @@ public class MainFrame extends JFrame{
 	}
 	
 	private void init() {
-		this.setTitle("Squirrel Baby's Dictionary");
+		this.setTitle("Dictionary");
 		int width = MainFrameConfig.WIDTH;
 		int height = MainFrameConfig.HEIGHT;
 		
